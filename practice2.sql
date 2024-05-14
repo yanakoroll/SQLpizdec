ae37a6294cec219dfbd07116970c5f4f641cd820
FOlkhoffski <148325615+FOlkhoffski@users.noreply.github.com>
2024-05-14 18:10:34 +0300

    Update practice2.sql

--- practice2.sql
+++ practice2.sql
@@ -1,1 +1,19 @@
+CREATE TABLE school (
+  snum INT PRIMARY KEY, 
+  sname VARCHAR(20), 
+);
 
+CREATE TABLE pupils (
+  pupnum INT PRIMARY KEY, 
+  snum INT REFERENCES schools, 
+  pupname VARCHAR(50) NOT NULL,
+  birth DATE, 
+  score NUMERIC(100)
+);
+
+CREATE TABLE parents (
+  pupnum INT REFERENCES pupils, 
+  parname VARCHAR(50),
+  sex CHAR(1), 
+  CHECK (sex IN ('м', 'ж'))
+);
